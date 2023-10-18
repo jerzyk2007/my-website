2b0c4c4f27bab252f74079155392f7952cdbd4d8
Jurek <jerzykomorowski77@gmail.com>
2023-10-18 19:20:57 +0200

    change index.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,20 +21,20 @@ const Navbar = () => {
                 <div className="navbar-container">
                     <Link to='/' className='navbar-logo'><DiAtom className='navbar-logo-atom' onClick={handleCloseMobileMenu} /><p className='navbar-logo-title' onClick={handleCloseMobileMenu}>Front Web Developer</p> </Link>
                     <ul className={mobileMenu ? 'navbar-menu active' : 'navbar-menu'}>
-                        <li className="nav-items">
-                            <Link to='/' className='nav-links' onClick={handleCloseMobileMenu}>Home</Link>
+                        <li className="navbar-items">
+                            <Link to='/' className='navbar-links' onClick={handleCloseMobileMenu}>Home</Link>
                         </li>
-                        <li className="nav-items">
-                            <Link to='/projects' className='nav-links' onClick={handleCloseMobileMenu}>My projects</Link>
+                        <li className="navbar-items">
+                            <Link to='/projects' className='navbar-links' onClick={handleCloseMobileMenu}>My projects</Link>
                         </li>
-                        <li className="nav-items">
-                            <Link to='https://github.com/jerzyk2007' className='nav-links' onClick={handleCloseMobileMenu}>GitHub</Link>
+                        <li className="navbar-items">
+                            <Link to='https://github.com/jerzyk2007' className='navbar-links' onClick={handleCloseMobileMenu}>GitHub</Link>
                         </li>
-                        <li className="nav-items">
-                            <Link to='/about-me' className='nav-links' onClick={handleCloseMobileMenu}>About Me</Link>
+                        <li className="navbar-items">
+                            <Link to='/about-me' className='navbar-links' onClick={handleCloseMobileMenu}>About Me</Link>
                         </li>
-                        {/* <li className="nav-items">
-                            <Link className='nav-links' onClick={handleCloseMobileMenu}>ENG/PL</Link>
+                        {/* <li className="navbar-items">
+                            <Link className='navbar-links' onClick={handleCloseMobileMenu}>ENG/PL</Link>
                         </li> */}
                     </ul>
                     <div className="navbar-menu-icon">
