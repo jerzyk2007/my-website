41a3da44ba1bcd418d9d7bd6f0488028ee7bab3d
Jurek <jerzykomorowski77@gmail.com>
2023-10-20 19:12:37 +0200

    changed Concept.css and deleted AboutMe.css

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,13 +1,12 @@
 import Concept from "../Concept";
 import Footer from "../Footer";
-import './AboutMe.css';
 
 const AboutMe = () => {
     return (
-        <div className='about-me'>
+        <>
             <Concept />
             <Footer />
-        </div>
+        </>
     );
 };
 
