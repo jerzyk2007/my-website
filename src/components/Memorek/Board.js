3b582553a880edc6b826b13e832fa3027b8ecbd9
Jurek <jerzykomorowski77@gmail.com>
2023-10-28 16:24:04 +0200

    created Learn.js

--- src/components/Memorek/Board.js
+++ src/components/Memorek/Board.js
@@ -1,30 +1,12 @@
-import { useContext, useEffect, useState } from 'react';
-import DataContext from './context/DataContext';
-import Questions from './Questions';
-import Answers from './Answers';
+import Learn from './Learn';
 import './Board.css';
 
 
 const Board = () => {
-    const { phrases, errorMessage } = useContext(DataContext);
-    const [phrase, setPhrase] = useState('');
-    const [blink, setBlink] = useState(true);
-
-
-    const handleDraw = () => {
-        if (phrases.length) {
-            setBlink(prev => !prev);
-            if (blink) {
-                const draw = Math.floor(Math.random() * phrases.length);
-                setPhrase(phrases[draw]);
-            }
-        }
-    };
 
     return (
         <div className='board'>
-            <Questions phrase={phrase} handleDraw={handleDraw} />
-            <Answers phrase={phrase} handleDraw={handleDraw} blink={blink} />
+            <Learn />
         </div>
     );
 };
---
+++ src/components/Memorek/Learn.js
@@ -0,0 +1,32 @@
+import { useContext, useEffect, useState } from 'react';
+import DataContext from './context/DataContext';
+import Questions from './Questions';
+import Answers from './Answers';
+import './Learn.css';
+
+
+const Learn = () => {
+    const { phrases, errorMessage } = useContext(DataContext);
+    const [phrase, setPhrase] = useState('');
+    const [blink, setBlink] = useState(true);
+
+
+    const handleDraw = () => {
+        if (phrases.length) {
+            setBlink(prev => !prev);
+            if (blink) {
+                const draw = Math.floor(Math.random() * phrases.length);
+                setPhrase(phrases[draw]);
+            }
+        }
+    };
+
+    return (
+        <div className='learn'>
+            <Questions phrase={phrase} handleDraw={handleDraw} />
+            <Answers phrase={phrase} handleDraw={handleDraw} blink={blink} />
+        </div>
+    );
+};
+
+export default Learn;
