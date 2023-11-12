5085799c298038e8009ffbe570c87980af3767c1
Jurek <jerzykomorowski77@gmail.com>
2023-11-12 11:29:50 +0100

    created hooks UseLogout and code

--- src/components/Memorek/UserSettings.js
+++ src/components/Memorek/UserSettings.js
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router";
 import useData from './hooks/useData';
+import useLogout from "./hooks/useLogout";
 import { Link } from 'react-router-dom';
 import { FiX } from "react-icons/fi";
 import './UserSettings.css';
@@ -7,6 +8,12 @@ import './UserSettings.css';
 const UserSettings = () => {
     const { auth } = useData();
     const navigate = useNavigate();
+    const logout = useLogout();
+
+    const handleLogout = async () => {
+        await logout();
+        navigate('/memorek');
+    };
 
     return (
         <section className='user_settings'>
@@ -21,7 +28,7 @@ const UserSettings = () => {
                 <button className="user_settings-button" >Change password</button>
             </Link>
             <Link to="/memorek/user-settings/logout" className="user_settings-link" >
-                <button className="user_settings-button" >Log out</button>
+                <button className="user_settings-button" onClick={handleLogout}>Log out</button>
             </Link>
 
             <FiX className='user_settings-close-button' onClick={() => navigate(-1)} />
